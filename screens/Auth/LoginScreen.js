92a4ba9d1c390cc3708efdc77cd5347a41796724
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2021-07-09 12:42:49 +0200

    adapt name of state and correct use of props

--- screens/Auth/LoginScreen.js
+++ screens/Auth/LoginScreen.js
@@ -3,9 +3,7 @@ import {
   ImageBackground,
   StyleSheet,
   Text,
-  TextInput,
   View,
-  TouchableOpacity,
 } from "react-native";
 
 import Input from "./../../components/Input"
@@ -14,46 +12,41 @@ import Button from "./../../components/Button"
 class LoginScreen extends Component {
   constructor(props) {
     super(props),
-      (this.state = { initials: "", password: "", });
+      (this.state = { username: "", password: "", });
       this.onUsernameChange = this.onUsernameChange.bind(this);
       this.onPasswordChange = this.onPasswordChange.bind(this);
   }
 
-  onUsernameChange = (initials) => {
-    this.setState({ initials: initials });
+  onUsernameChange(username){
+    this.setState({ username: username })
   };
 
-  onPasswordChange = (password) => {
-    this.setState({ password: password });
+  onPasswordChange(password){
+    this.setState({ password: password })
   };
 
   onPressLogin() {
-    const { initials, password } = this.state;
-    const payload = { initials, password };
+    const { username, password } = this.state;
+    const payload = { username, password };
 
     const onSuccess = ({ data }) => {
       this.setState({ userToken: data.token });
       localStorage.setItem("user_token", this.state.userToken);
       localStorage.setItem("base", this.state.base);
-      localStorage.setItem("nav", "Home");
+      localStorage.setItem("nav", "Home");this.props.navigation.navigate("Login")
       this.props.auth(data.token);
     };
 
     const onFailure = (error) => {
       console.log(error && error.response);
       showMessage({
-        message: "Les initiales ou le mot de passe est incorrect.",
+        message: "Le nom de l'utilisateur ou le mot de passe est incorrect.",
         type: "danger",
         duration: 6000
       });
     };
-    this.state.base == "" ?
-      showMessage({
-        message: "aucune base n'a été sélectionnée.",
-        type: "warning",
-        duration: 6000
-      }) : APIKit.getToken(payload).then(onSuccess).catch(onFailure);
   }
+  
 
   render() {
     return (
@@ -65,7 +58,7 @@ class LoginScreen extends Component {
           <View style={styles.areaLogin}>
             <Input placeholder="Nom d'utilisateur" onChange={this.onUsernameChange} background="#FBEFAE" color="#684500"/>
             <Input placeholder="Mot de passe" onChange={this.onPasswordChange} background="#FBEFAE" color="#684500" security={true}/>
-            <Button action={this.props.navigation.navigate("Login")} textButton="Se connecter" background="#684500" color="#FFFFFF"/>
+            <Button action={this.onPressLogin()} textButton="Se connecter" background="#684500" color="#FFFFFF"/>
           </View>
         </ImageBackground>
       </View>
